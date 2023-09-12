1434a06e2654c6c62ee055254d8a57e92aa1e4ec
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-09-12 20:49:30 +0800

    Add files via upload

---
+++ 顺序查找.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+typedef struct {
+	int* elem;
+	int TableLen;
+}SSTable;
+int search_Seq(SSTable& ST, int key)
+{
+	ST.elem[0] = key;
+	int i;
+	for (i = ST.TableLen; ST.elem[i] != key; i--);
+	printf("ret=%d\n", i);
+	return i;
+}
+int main()
+{
+	SSTable ST;
+	ST.elem = new int[20];
+	for (int i = 1; i <= 10; i++)
+	{
+		ST.elem[i] = i;
+	}
+	ST.TableLen = 10;
+	int key = 4;
+	int ret = search_Seq(ST, key);
+	printf("ret=%d\n", ret);
+	return 0;
+}
