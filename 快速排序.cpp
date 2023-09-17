43f24dc5a648b849cc16cccbd40ab350fe73f755
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-09-17 12:47:41 +0800

    Add files via upload

---
+++ 快速排序.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int partition(int A[], int low, int high)
+{
+	int pivot = A[low];
+	while (low < high)
+	{
+		while (low < high && A[high] >= pivot) --high;
+		A[low] = A[high];
+		while (low < high && A[low] <= pivot) low++;
+		A[high] = A[low];
+	}
+	A[low] = pivot;
+	return low;//return high;
+}
+void QuickSort(int A[], int low, int high)
+{
+	if (low < high)
+	{
+		int pivotpos = partition(A, low, high);
+		QuickSort(A, low, pivotpos - 1);
+		QuickSort(A, pivotpos + 1, high);
+	}
+}
+int main()
+{
+	int A[] = { NULL,23,21,62,24,23,26,43,14,28,8,9,19,11 };
+	QuickSort(A, 1, 13);
+	for (int i = 1; i <= 13; i++)
+	{
+		printf("%d ", A[i]);
+	}
+	return 0;
+}
