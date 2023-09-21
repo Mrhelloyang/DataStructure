7962bc65845aedd08bb0cf1e0d9f6b1e4dc64732
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-09-21 21:43:53 +0800

    Add files via upload

---
+++ 顺序栈的基本操作.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#define MaxSize 50
+typedef struct {
+	int data[MaxSize];
+	int top;//栈顶指针
+}SqStack;
+void InitStack(SqStack& S)
+{
+	S.top = -1;
+}
+bool StackEmpty(SqStack& S)
+{
+	if (S.top == -1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool push(SqStack& S, int x)
+{
+	if (S.top == MaxSize - 1)
+		return false;
+	//S.top++;
+	//S.data[S.top] = x;
+	S.data[++S.top] = x;
+	return true;
+}
+bool Pop(SqStack& S, int& x)
+{
+	if (S.top == -1)
+		return false;
+	x = S.data[S.top--];
+	return true;
+}
+
+int main()
+{
+	SqStack S;
+	InitStack(S);
+	int x;
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &x);
+		push(S, x);
+	}
+	int tmp;
+	for (int i = 0; i < 10; i++)
+	{
+		Pop(S, tmp);
+		printf("%d\n", tmp);
+	}
+	return 0;
+}
